e42809f88c9fc4f87753266b6c8e45eca2ec9b2f
Strange <rivasjoaquin02@gmail.com>
2024-01-16 10:33:47 -0500

    feat(app): added chat component

--- app/app/chat/page.tsx
+++ app/app/chat/page.tsx
@@ -1,5 +1,10 @@
+import { Chat } from "@/components/chat/chat";
 
 export default function Page() {
 
-    return <main>chat page</main>
+    return <main className="h-full">
+        <div className="w-full h-full gap-4 mx-auto max-w-screen-xl px-4 flex flex-col stretch">
+            <Chat />
+        </div>
+    </main>
 }
