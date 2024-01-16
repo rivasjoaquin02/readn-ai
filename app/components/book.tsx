711497583f2e6707de84f44d0ab92daa3a92a33b
Strange <rivasjoaquin02@gmail.com>
2024-01-16 18:49:48 -0500

    refactor(app): now you can change the classNames of the book component

--- app/components/book.tsx
+++ app/components/book.tsx
@@ -3,31 +3,47 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { StarIcon } from "lucide-react";
 import { toggleFavorite } from "@/lib/actions";
+import { cn } from "@/lib/utils";
 
-export default function BookCard({ children }: { children: React.ReactNode }) {
-    return <div className="flex flex-col items-center">{children}</div>;
+export default function BookCard({
+    className,
+    children
+}: {
+    className?: string;
+    children: React.ReactNode;
+}) {
+    return (
+        <div className={cn("flex flex-col items-center", className)}>
+            {children}
+        </div>
+    );
 }
 
 const BookImage = ({
     id,
     href,
     src,
-    alt
+    alt,
+    className
 }: {
     id: string;
     href: string;
     src: string;
     alt: string;
+    className?: string;
 }) => {
     const userId = "550e8400-e29b-41d4-a716-446655440000";
     const toggleFavoriteWithId = toggleFavorite.bind(null, userId, id);
 
     return (
-        <div className="relative transform-gpu transition-transform hover:-translate-y-1 hover:scale-105">
+        <div
+            className={cn(
+                "relative h-full w-full transform-gpu transition-transform hover:-translate-y-1 hover:scale-105",
+                className
+            )}
+        >
             <Link href={href}>
-                <div
-                    className="h-[280px] w-[200px] cursor-pointer bg-yellow-600 shadow-2xl shadow-gray-700"
-                >
+                <div className="h-full w-full cursor-pointer bg-yellow-600 shadow-2xl shadow-gray-700">
                     {/*<Image src={src} alt={alt} width={200} height={400} />*/}
                 </div>
             </Link>
