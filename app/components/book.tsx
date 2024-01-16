3b6647f17bad8861380a6ea2169a534c3a1ffc86
Strange <rivasjoaquin02@gmail.com>
2024-01-16 10:39:57 -0500

    refactor(app): big changes in the component

--- app/components/book.tsx
+++ app/components/book.tsx
@@ -1,27 +1,56 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { StarIcon } from "lucide-react";
+import { toggleFavorite } from "@/lib/actions";
 
 export default function BookCard({ children }: { children: React.ReactNode }) {
-    return <div className="cursor-pointer group flex flex-col justify-center items-center">
-        {children}
-    </div>
+    return <div className="flex flex-col items-center">{children}</div>;
 }
 
-const BookImage = ({src, alt}: {src: string, alt: string}) => {
-    //return <Image src={src} alt={alt} width={200} height={400} />
-    return <div className="w-[200px] h-[280px] group-hover:scale-105 group-hover:rotate-2 transition-transform transform-gpu bg-yellow-600 shadow-2xl shadow-gray-700">{alt}</div>
-}
+const BookImage = ({
+    id,
+    href,
+    src,
+    alt
+}: {
+    id: string;
+    href: string;
+    src: string;
+    alt: string;
+}) => {
+    const userId = "550e8400-e29b-41d4-a716-446655440000";
+    const toggleFavoriteWithId = toggleFavorite.bind(null, userId, id);
+
+    return (
+        <div className="relative transform-gpu transition-transform hover:-translate-y-1 hover:scale-105">
+            <Link href={href}>
+                <div
+                    className="h-[280px] w-[200px] cursor-pointer bg-yellow-600 shadow-2xl shadow-gray-700"
+                >
+                    {/*<Image src={src} alt={alt} width={200} height={400} />*/}
+                </div>
+            </Link>
+            <form action={toggleFavoriteWithId}>
+                <Button
+                    variant="outline"
+                    className="absolute bottom-2 right-2 z-10 h-8 w-8 cursor-pointer p-0"
+                >
+                    <StarIcon className="h-4 w-4" />
+                </Button>
+            </form>
+        </div>
+    );
+};
 
 const BookInfo = ({ children }: { children: React.ReactNode }) => {
-    return <div className="flex flex-col py-4">
-        {children}
-    </div>
-}
+    return <div className="flex flex-col py-4">{children}</div>;
+};
 
 const BookTitle = ({ children }: { children: React.ReactNode }) => {
-    return <h3 className="font-semibold text-center">{children}</h3>
-}
+    return <h3 className="text-center font-semibold">{children}</h3>;
+};
 
-BookCard.Image = BookImage
-BookCard.Info = BookInfo
-BookCard.Title = BookTitle
-
+BookCard.Image = BookImage;
+BookCard.Info = BookInfo;
+BookCard.Title = BookTitle;
