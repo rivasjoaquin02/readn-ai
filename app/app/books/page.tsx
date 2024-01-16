c61c76986422fa12ffa024fbfbebf3a531371a93
Strange <rivasjoaquin02@gmail.com>
2024-01-16 18:49:20 -0500

    feat(app): implemented new feature for filter recent books

--- app/app/books/page.tsx
+++ app/app/books/page.tsx
@@ -2,23 +2,31 @@ import { ListBooks } from "@/components/list-books";
 import { Pagination } from "@/components/pagination";
 import { getBookPages } from "@/lib/data";
 
-
-export default async function Page({ searchParams }: {
+export default async function Page({
+    searchParams
+}: {
     searchParams?: {
         query?: string;
         page?: string;
-    }
+        recent?: "asc" | "desc"  
+    };
 }) {
-    const query = searchParams?.query || ''
-    const currentPage = Number(searchParams?.page) || 1
-    const totalPages = await getBookPages(query)
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+    const recent = searchParams?.recent
+    const totalPages = await getBookPages(query);
 
-    return <main>
-
-        <ul className="grid gap-4" style={{ gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))" }}>
-            <ListBooks query={query} currentPage={currentPage} />
-        </ul>
-        <Pagination totalPages={totalPages} />
-    </main>
-
+    return (
+        <main>
+            <ul
+                className="grid gap-4"
+                style={{
+                    gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))"
+                }}
+            >
+                <ListBooks query={query} currentPage={currentPage} recent={recent} />
+            </ul>
+            <Pagination totalPages={totalPages} />
+        </main>
+    );
 }
